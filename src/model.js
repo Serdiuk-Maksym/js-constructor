ee43536269c34d425ca00bd9c1d92bd626a434fa
Serdiuk-Maksym <serdyukmaksimaleksandrovich@gmail.com>
2023-07-05 21:42:52 +0300

    second

---
+++ src/model.js
@@ -0,0 +1,9 @@
+export const model = [
+  { type: 'title', value: 'Hello World from js' },
+  { type: 'text', value: 'here we go with some text' },
+  {
+    type: 'columns',
+    value: ['11111', '22222', '33333'],
+  },
+  { type: 'image', value: './assets/image.png' },
+];
